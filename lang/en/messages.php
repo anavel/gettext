20b5e43d9cff5ded2fbef1ec4944287b19f89c5c
Andrea <andrea@anavallasuiza.com>
2016-07-12 11:50:13 +0200

    Update messages.php

--- lang/en/messages.php
+++ lang/en/messages.php
@@ -1,8 +1,10 @@
 <?php
 
 return [
-    'edit_title' => 'Edit',
-    'alert_success_update_title' => 'Everything OK!',
-    'alert_success_update_text' => 'The entries were updated successfully',
-    'save_button' => 'Save'
+    'edit_title'                    => 'Edit',
+    'alert_success_update_title'    => 'Everything OK!',
+    'alert_success_update_text'     => 'These entries were successfully updated.',
+    'Only empty'                    => 'Only empty',
+    'save_button'                   => 'Save'
+    'Search (min. 3 charts)...'     => 'Search (min. 3 charts)...',
 ];
