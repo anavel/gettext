ec61c8e78faeb7d4557aab307d27c4be7856dbea
Andrea <andrea@anavallasuiza.com>
2016-07-12 11:48:53 +0200

    Update messages.php

--- lang/es/messages.php
+++ lang/es/messages.php
@@ -1,11 +1,7 @@
 <?php
 
 return [
-  'ca_ES'                            => 'CAT',
   'edit_title'                       => 'Textos de navegación',
-  'es_ES'                            => 'ESP',
-  'eu_ES'                            => 'EUS',
-  'en_ES'                            => 'ENG',
   'Only empty'                       => 'Solo vacíos',
   'save_button'                      => 'Guardar',
   'Search (min. 3 charts)...'        => 'Buscar por (mín. 3 caracteres)...',
