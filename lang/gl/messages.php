0dbb5e0674142c8f2234ad2452e538675f88c72d
Andrea <andrea@anavallasuiza.com>
2016-07-12 11:48:13 +0200

    Update messages.php

--- lang/gl/messages.php
+++ lang/gl/messages.php
@@ -1,11 +1,7 @@
 <?php
 
 return [
-  'ca_ES'                            => 'CAT',
   'edit_title'                       => 'Textos de navegación',
-  'es_ES'                            => 'CAST',
-  'eu_ES'                            => 'EUS',
-  'en_ES'                            => 'ENG',
   'Only empty'                       => 'Só baleiros',
   'save_button'                      => 'Gardar',
   'Search (min. 3 charts)...'        => 'Buscar por (mín. 3 caracteres)...',
