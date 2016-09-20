dfe9de900d0db029b6655eeebc8f3981983d7e55
Adrian P. Blunier <adrian.blunier@gmail.com>
2016-09-20 11:36:31 +0200

    Fixed sintax error

--- lang/en/messages.php
+++ lang/en/messages.php
@@ -5,6 +5,6 @@ return [
     'alert_success_update_title'    => 'Everything OK!',
     'alert_success_update_text'     => 'These entries were successfully updated.',
     'Only empty'                    => 'Only empty',
-    'save_button'                   => 'Save'
+    'save_button'                   => 'Save',
     'Search (min. 3 charts)...'     => 'Search (min. 3 charts)...',
 ];
