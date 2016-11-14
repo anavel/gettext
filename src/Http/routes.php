bd0299635dd08fa56bf3ba99da4cdb30ae36c438
ablunier <adrian.blunier@gmail.com>
2016-11-14 09:14:43 +0000

    Applied fixes from StyleCI

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -2,18 +2,18 @@
 
 Route::group(
     [
-        'prefix' => 'gettext',
-        'namespace' => 'Anavel\Gettext\Http\Controllers'
+        'prefix'    => 'gettext',
+        'namespace' => 'Anavel\Gettext\Http\Controllers',
     ],
     function () {
         Route::get('/edit/{locale?}', [
             'as'   => 'anavel-gettext.edit',
-            'uses' => 'MainController@edit'
+            'uses' => 'MainController@edit',
         ]);
 
         Route::put('/{locale}', [
             'as'   => 'anavel-gettext.update',
-            'uses' => 'MainController@update'
+            'uses' => 'MainController@update',
         ]);
     }
 );
